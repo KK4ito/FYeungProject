672b0326a9c6cb297aeaae69201d90d38a63c0ea
Felix <felix.yeung@icloud.com>
2016-05-22 19:40:52 +0200

    made 2 new histograms only for 2 Var. !

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -15,10 +15,7 @@ import javafx.scene.chart.*;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -27,10 +24,15 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLClientInfoException;
 import java.util.*;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addComponentListener;
+import static java.lang.StrictMath.ceil;
 
 public class App extends Application {
 
@@ -44,10 +46,11 @@ public class App extends Application {
 	 LineChart<Number, Number> lineChart ;
 	@Override
 	public void start(Stage stage) {
+		StackPane pane = new StackPane();
 
-		//	FileChooser filechooser = new FileChooser();
-		//	File file = filechooser.showOpenDialog(stage);
-		File file = new File("bin/helvetia.txt");
+			FileChooser filechooser = new FileChooser();
+			File file = filechooser.showOpenDialog(stage);
+		//File file = new File("bin/helvetia.txt");
 		//	File file = new File("bin/temperatur-basel-all.txt");
 
 		FileParser fp;
@@ -55,12 +58,13 @@ public class App extends Application {
 
 			fp = getData(file);
 			fp.readData(file);
-			Button button;
+			Label label;
+
 			if(file.exists()){
-				button = new Button("Found");
+				label = new Label("Found");
 			}
 			else {
-				button = new Button("NA");
+				label = new Label("NA");
 			}
 
 
@@ -109,25 +113,50 @@ public class App extends Application {
             ScatterChart<Number, Number> sc = getScatterChart(file, fp);
 
 
-
-
-
             lineChart.getData().addAll(createChartData(fp.getList()));
 
 
+
+
+			CategoryAxis xAxis = new CategoryAxis();
+			NumberAxis yAxis = new NumberAxis();
+			BarChart barChart = new BarChart(xAxis, yAxis);
+			barChart.setData(createBarChartData(fp.getList(),0));
+			barChart.setLegendVisible(false);
+			xAxis.setTickLabelsVisible(false);
+			barChart.setBarGap(0);
+			barChart.setCategoryGap(0);
+			barChart.setCategoryGap(0);
+			barChart.prefWidth(1000);
+
+
+
+
+
+
+			CategoryAxis xAxis2 = new CategoryAxis();
+			NumberAxis yAxis2 = new NumberAxis();
+			BarChart barChart2 = new BarChart(xAxis2, yAxis2);
+			barChart2.setData(createBarChartData(fp.getList(),1));
+			barChart2.setLegendVisible(false);
+			xAxis2.setTickLabelsVisible(false);
+			barChart2.setBarGap(0);
+			barChart2.setCategoryGap(0);
+			barChart2.prefWidth(1000);
+
 			HBox firstLine = new HBox();
-			firstLine.getChildren().addAll(cb,cb2, button,checkbox);
+			firstLine.getChildren().addAll(cb,cb2, label,checkbox);
 			firstLine.setAlignment(Pos.CENTER);
 			firstLine.setSpacing(10);
 			firstLine.setPadding(new Insets(5, 5, 5, 5));
 
-			/*
+
 			HBox scatterChartLine = new HBox();
-			scatterChartLine.getChildren().addAll(sc);
-			scatterChartLine.setSpacing(10);
+			scatterChartLine.getChildren().addAll(barChart, barChart2);
+			scatterChartLine.setSpacing(1);
 			scatterChartLine.setPadding(new Insets(5, 5, 5, 5));
 
-
+/*
 			HBox histogramLine = new HBox();
 			histogramLine.getChildren().addAll();
 			histogramLine.setAlignment(Pos.CENTER);
@@ -137,19 +166,22 @@ public class App extends Application {
 
 			root = new StackPane();
 
+
 			root.getChildren().addAll(sc, lineChart);
+
+
 
 			System.out.println(sc.getData().get(0).getData());
 			System.out.println(lineChart.getData().get(0).getData());
 			VBox vBox = new VBox();
 			//vBox.getChildren().addAll(firstLine,scatterChartLine,histogramLine);
-			vBox.getChildren().addAll(firstLine,root);
+			vBox.getChildren().addAll(firstLine,root,scatterChartLine);
 			vBox.setAlignment(Pos.CENTER);
 			vBox.setSpacing(10);
 			vBox.setPadding(new Insets(5, 5, 5, 5));
 
 
-			StackPane pane = new StackPane();
+
 			pane.getChildren().add(vBox);
 
 			Scene scene = new Scene(pane, 800, 800);
@@ -168,7 +200,8 @@ public class App extends Application {
     private ScatterChart<Number, Number> getScatterChart(File file, FileParser fp) {
         ScatterChart<Number,Number> sc = new ScatterChart<>(scxAxis,scyAxis);
 
-        //sc.setTitle(file.getName());
+        /*sc.setTitle(file.getName());
+        lineChart.setTitle(file.getName());*/
         sc.setPrefSize(1000,600);
         sc.setLegendVisible(false);
         sc.getData().addAll(createChartData(fp.getList()));
@@ -220,6 +253,44 @@ public class App extends Application {
 		}
 		System.out.println("ERROR");
 		return null;
+	}
+
+	private ObservableList<Series<String, Double>> createBarChartData(List<DataModel> lst, int select) {
+
+		ObservableList<Series<String, Double>> answer = FXCollections.observableArrayList();
+		Series<String, Double> aSeries = new Series<String, Double>();
+
+		DataModel dm1 = lst.get(select);
+		//DataModel dm2 = lst.get(1);
+
+
+		Double max = Collections.max(dm1.getValues());
+		Double min = Collections.min((dm1.getValues()));
+		double range = ceil(Math.sqrt(dm1.getValues().size()));
+		double width = Math.abs((max-min)/range);
+		int n=0;
+		int count = 0;
+		int testCount =0;
+		for (int i = 0; i < range; i++) {
+			for (int m = 0 ; m < dm1.getValues().size(); m++){
+
+				if(min+width*i<=dm1.getValue(m) && min+width*(i+1)>=dm1.getValue(m)){
+					count++;
+				}
+			}
+			n++;
+
+			System.out.println(count);
+			aSeries.getData().add(new XYChart.Data(Integer.toString(i), count));
+			testCount+=count;
+			count=0;
+		}
+		System.out.println(dm1.getValues().size());
+		System.out.println("range:" +range);
+		System.out.println("count:" +testCount);
+		answer.addAll(aSeries);
+
+		return answer;
 	}
 
 
